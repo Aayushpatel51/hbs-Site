0ace865b64872c854c6fc3a12591a785f9f9530d
aayush <64529257+Aayushpatel51@users.noreply.github.com>
2021-07-04 10:02:30 +0530

    Initial commit

---
+++ src/db/conn.js
@@ -0,0 +1,12 @@
+const { text } = require("express");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/dynamicSite",{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>{
+    console.log("connection successful")
+}).catch((error)=>{
+    console.log(error);
+})
---
+++ src/models/userdata.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+ const validator = require("validator");
+
+ const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        validator(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid");
+            }
+        }
+    },
+    phone:{
+        type:Number,
+        required:true,
+        min:10
+    }
+})
+
+const User = mongoose.model("User",userSchema);
+module.exports = User;
